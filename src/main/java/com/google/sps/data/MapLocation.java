103f7db634b7159049ff31d61a7da48a2a00aab6
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-06-25 17:41:20 +0000

    Change lat/lng type

--- src/main/java/com/google/sps/data/MapLocation.java
+++ src/main/java/com/google/sps/data/MapLocation.java
@@ -2,16 +2,16 @@ package com.google.sps.data;
 
 /** Class representing a MapLocation as a coordinate **/
 public class MapLocation {
-  public long lat;
-  public long lng;
+  public double lat;
+  public double lng;
   
   /** Creates a new MapLocation
   * @param lat latitude value
   * @param lng longitude value
   * @return MapLocation object
   **/
-  
-  public MapLocation (long lat, long lng) {
+
+  public MapLocation (double lat, double lng) {
     this.lat = lat;
     this.lng = lng;
   }
