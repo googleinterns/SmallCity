3bd3403429f83191cdae1398b11f9011a96d547e
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-06-26 16:10:59 +0000

    Add javadoc

--- src/main/java/com/google/sps/data/SmallCityService.java
+++ src/main/java/com/google/sps/data/SmallCityService.java
@@ -1,10 +1,15 @@
 package com.google.sps.data;
 
+/** SmallCityService object representing all components of the webapp **/
 public class SmallCityService {
   
   private User user;
   private List<Listing> businessList;
-
+  
+  /** Create a new Small City Service instance
+  * @param user User of webapp with mapLocation
+  * @return List of small local businesses
+  **/
   public SmallCityService(User user) {
     this.user = user;
     getAllBusinesses();
