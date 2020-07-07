74836e309aa311dfa69c61713ce90f84e7c41878
AbakirH <abakir@google.com>
2020-07-07 16:33:44 +0000

    created a local database to be used when testing functionality of the app

--- src/test/java/com/google/sps/BigBusinessesTest.java
+++ src/test/java/com/google/sps/BigBusinessesTest.java
@@ -20,13 +20,65 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Iterator;
+
+import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 @RunWith(JUnit4.class)
 public final class BigBusinessesTest {
   private MapLocation googlesOffice = new MapLocation(40.457177, -79.916696);
-  private List<Listing> sampleListOfBusinesses = new LinkedList<>();
-  private List<Listing> expectedBusinesses = new LinkedList<>();
+  private List<Listing> sampleListOfBusinesses = new LinkedList<>();  
   
+  private final LocalServiceTestHelper helper =
+    new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  
+  private void setUpSampleDatabase() {
+    String title = "business";
+    Entity taskEntity = new Entity("BigBusinesses");
+    String[] businesses = {"McDonalds","west elm","LA Fitness"};
+
+   
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    for(String business: businesses){
+      taskEntity = new Entity("BigBusinesses");
+      taskEntity.setProperty(title, business);
+      datastore.put(taskEntity);
+    }
+    assertEquals(3, datastore.prepare(new Query("BigBusinesses")).countEntities(withLimit(10)));
+   
+  }
+
+  // To be used to see if the sample database is able to create a 
+  // local one to be used when running the command mvn test
+  @Test
+  public void testSampleDatabase() {
+    setUpSampleDatabase();
+  }
+
   @Test
   public void testBusinesses() {
     sampleListOfBusinesses.add(new Listing("LA Fitness", new MapLocation(40.457091, -79.915331), 3.9, null, "https://www.lafitness.com/Pages/Default.aspx"));
@@ -35,15 +87,16 @@ public final class BigBusinessesTest {
     sampleListOfBusinesses.add(new Listing("East End Brewing Company", new MapLocation(40.459391, -79.911782), 4.7, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eastendbrewing.com%2F&psig=AOvVaw0kX_SAlxhA09EN3cKpt5ik&ust=1593613487774000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDA6oDfqeoCFQAAAAAdAAAAABAD", "http://www.eastendbrewing.com/"));
     sampleListOfBusinesses.add(new Listing("The Shiny Bean Coffee & Tea", new MapLocation(40.496328, -79.944862), 4.9, "https://goo.gl/maps/AYH2QCL7pkoMBxHA8", "https://theshinybean.com/"));
     sampleListOfBusinesses.add(new Listing("Weisshouse", new MapLocation(40.456684, -79.925499), 4.3, "https://goo.gl/maps/7tuXn7QF2hh7ioGYA", "https://www.weisshouse.com/"));
- 
+
     SmallCityService testServiceUser = new SmallCityService(googlesOffice);
     testServiceUser.setAllBusinesses(sampleListOfBusinesses);
-    
+
     sampleListOfBusinesses = testServiceUser.eliminateBigBusinesses();
-    
+
     expectedBusinesses.add(new Listing("East End Brewing Company", new MapLocation(40.459391, -79.911782), 4.7, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eastendbrewing.com%2F&psig=AOvVaw0kX_SAlxhA09EN3cKpt5ik&ust=1593613487774000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDA6oDfqeoCFQAAAAAdAAAAABAD", "http://www.eastendbrewing.com/"));
     expectedBusinesses.add(new Listing("The Shiny Bean Coffee & Tea", new MapLocation(40.496328, -79.944862), 4.9, "https://goo.gl/maps/AYH2QCL7pkoMBxHA8", "https://theshinybean.com/"));
     expectedBusinesses.add(new Listing("Weisshouse", new MapLocation(40.456684, -79.925499), 4.3, "https://goo.gl/maps/7tuXn7QF2hh7ioGYA", "https://www.weisshouse.com/"));
     Assert.assertEquals(expectedBusinesses, sampleListOfBusinesses);
   }
+  
 }
