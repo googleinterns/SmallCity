963866d561e4c51b86650e2b88b45a22d24e2d74
AbakirH <abakir@google.com>
2020-07-07 16:36:26 +0000

    fixed the logic when removing the big businesses from the sample list

--- src/test/java/com/google/sps/BigBusinessesTest.java
+++ src/test/java/com/google/sps/BigBusinessesTest.java
@@ -81,22 +81,23 @@ public final class BigBusinessesTest {
 
   @Test
   public void testBusinesses() {
+    setUpSampleDatabase();
+
     sampleListOfBusinesses.add(new Listing("LA Fitness", new MapLocation(40.457091, -79.915331), 3.9, null, "https://www.lafitness.com/Pages/Default.aspx"));
     sampleListOfBusinesses.add(new Listing("west elm", new MapLocation(40.456279, -79.915015), 3.6, null, "https://www.westelm.com"));
     sampleListOfBusinesses.add(new Listing("McDonalds", new MapLocation(40.459450, -79.918479), 2.6, null, "https://www.mcdonalds.com"));
     sampleListOfBusinesses.add(new Listing("East End Brewing Company", new MapLocation(40.459391, -79.911782), 4.7, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eastendbrewing.com%2F&psig=AOvVaw0kX_SAlxhA09EN3cKpt5ik&ust=1593613487774000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDA6oDfqeoCFQAAAAAdAAAAABAD", "http://www.eastendbrewing.com/"));
     sampleListOfBusinesses.add(new Listing("The Shiny Bean Coffee & Tea", new MapLocation(40.496328, -79.944862), 4.9, "https://goo.gl/maps/AYH2QCL7pkoMBxHA8", "https://theshinybean.com/"));
     sampleListOfBusinesses.add(new Listing("Weisshouse", new MapLocation(40.456684, -79.925499), 4.3, "https://goo.gl/maps/7tuXn7QF2hh7ioGYA", "https://www.weisshouse.com/"));
-
+ 
     SmallCityService testServiceUser = new SmallCityService(googlesOffice);
     testServiceUser.setAllBusinesses(sampleListOfBusinesses);
+    
+    sampleListOfBusinesses.remove(0);
+    sampleListOfBusinesses.remove(1);
+    sampleListOfBusinesses.remove(2);
 
-    sampleListOfBusinesses = testServiceUser.eliminateBigBusinesses();
-
-    expectedBusinesses.add(new Listing("East End Brewing Company", new MapLocation(40.459391, -79.911782), 4.7, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eastendbrewing.com%2F&psig=AOvVaw0kX_SAlxhA09EN3cKpt5ik&ust=1593613487774000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDA6oDfqeoCFQAAAAAdAAAAABAD", "http://www.eastendbrewing.com/"));
-    expectedBusinesses.add(new Listing("The Shiny Bean Coffee & Tea", new MapLocation(40.496328, -79.944862), 4.9, "https://goo.gl/maps/AYH2QCL7pkoMBxHA8", "https://theshinybean.com/"));
-    expectedBusinesses.add(new Listing("Weisshouse", new MapLocation(40.456684, -79.925499), 4.3, "https://goo.gl/maps/7tuXn7QF2hh7ioGYA", "https://www.weisshouse.com/"));
-    Assert.assertEquals(expectedBusinesses, sampleListOfBusinesses);
+    Assert.assertEquals(sampleListOfBusinesses, testServiceUser.eliminateBigBusinesses());
   }
   
 }
