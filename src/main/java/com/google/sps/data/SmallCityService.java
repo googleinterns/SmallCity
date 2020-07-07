ba759a9ccad7e98c2fd783d7b780433b55a0e7de
AbakirH <abakir@google.com>
2020-07-07 16:45:53 +0000

    completed the eleminateBigBusinesses method

--- src/main/java/com/google/sps/data/SmallCityService.java
+++ src/main/java/com/google/sps/data/SmallCityService.java
@@ -1,12 +1,20 @@
 package com.google.sps.data;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Iterator;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 
 /** SmallCityService object representing all components of the webapp **/
 public class SmallCityService {
   
   private User user;
-  private List<Listing> businesses;
+  private List<Listing> businesses = new LinkedList<>();
   
   /** Create a new Small City Service instance
   * @param mapLocation geolocation of user
@@ -24,12 +32,28 @@ public class SmallCityService {
   }
   
   // Function is only here until the places API is implemented,
-  // and for test purposes
+  // and for test file to be able to set any static business LinkedList we want
+  // to try to use
   public void setAllBusinesses(List<Listing> allBusinesses){
-    businesses = allBusinesses;
+   businesses = allBusinesses;
   }
 
-  public void eliminateBigBusinesses() {
-    // TODO: Parse big business list and remove big businesses from businessList
+  public List<Listing> eliminateBigBusinesses() {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query("BigBusinesses").addSort("business", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+    Iterator<Listing> businessesList =  businesses.iterator();
+    Listing tempListing;
+    while (businessesList.hasNext()){
+      tempListing = businessesList.next();
+      for (Entity entity : results.asIterable()) {
+        String businessName = (String) entity.getProperty("business");
+        if(businessName.equals(tempListing.getName())){
+          businessesList.remove();
+          break;
+        }
+      }
+    }
+    return businesses;
   }
 }
