b4a10189f8c0ae70ac50f67bc752880f98506252
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-07-08 18:56:00 +0000

    Implement Places API nearby search request

--- src/main/java/com/google/sps/data/SmallCityService.java
+++ src/main/java/com/google/sps/data/SmallCityService.java
@@ -1,12 +1,29 @@
 package com.google.sps.data;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Arrays;
+import java.io.Serializable;
+import java.net.URL;
+import java.util.logging.Logger;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PlacesApi;
+import com.google.maps.NearbySearchRequest;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.LocationType;
+import com.google.maps.model.Photo;
+import com.google.maps.model.PlaceType;
+import com.google.maps.model.RankBy;
+import com.google.maps.model.Geometry;
+import com.google.maps.model.PlacesSearchResponse;
+import com.google.maps.model.PlacesSearchResult;
 
 /** SmallCityService object representing all components of the webapp **/
 public class SmallCityService {
   
   private User user;
   private List<Listing> businesses;
+  private final String KEY = "REDACTED";
+  private final static Logger LOGGER = Logger.getLogger(SmallCityService.class.getName());
   
   /** Create a new Small City Service instance
   * @param mapLocation geolocation of user
@@ -19,14 +36,27 @@ public class SmallCityService {
   }
 
   public void findAllBusinesses() {
-    // TODO: Get businesses from Place API given user location
     businesses = new LinkedList<Listing>();
-    businesses.add(new Listing("LA Fitness", new MapLocation(40.457091, -79.915331), 3.9, null, "https://www.lafitness.com/Pages/Default.aspx"));
-    businesses.add(new Listing("west elm", new MapLocation(40.456279, -79.915015), 3.6, null, "https://www.westelm.com"));
-    businesses.add(new Listing("McDonalds", new MapLocation(40.459450, -79.918479), 2.6, null, "https://www.mcdonalds.com"));
-    businesses.add(new Listing("East End Brewing Company", new MapLocation(40.459391, -79.911782), 4.7, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eastendbrewing.com%2F&psig=AOvVaw0kX_SAlxhA09EN3cKpt5ik&ust=1593613487774000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLDA6oDfqeoCFQAAAAAdAAAAABAD", "http://www.eastendbrewing.com/"));
-    businesses.add(new Listing("The Shiny Bean Coffee & Tea", new MapLocation(40.496328, -79.944862), 4.9, "https://goo.gl/maps/AYH2QCL7pkoMBxHA8", "https://theshinybean.com/"));
-    businesses.add(new Listing("Weisshouse", new MapLocation(40.456684, -79.925499), 4.3, "https://goo.gl/maps/7tuXn7QF2hh7ioGYA", "https://www.weisshouse.com/"));
+    LatLng latLng = new LatLng(user.getGeolocation().lat, user.getGeolocation().lng);
+    final GeoApiContext context = new GeoApiContext.Builder()
+            .apiKey(KEY)
+            .build();
+    NearbySearchRequest request = PlacesApi.nearbySearchQuery(context, latLng);
+    try {
+      PlacesSearchResponse response = request.type(PlaceType.STORE)
+              .rankby(RankBy.DISTANCE)
+              .await();
+      final int allowedSearchRequests = 3;
+      for (int i=0; i<allowedSearchRequests; i++) {
+        for(PlacesSearchResult place : response.results) {
+          addListingToBusinesses(place);
+        }
+        Thread.sleep(2000); // Required delay before next API request
+	      response = PlacesApi.nearbySearchNextPage(context, response.nextPageToken).await();
+      }
+    } catch(Exception e) {
+      LOGGER.warning(e.getMessage());
+    }  
   }
 
   public void eliminateBigBusinesses() {
@@ -36,5 +66,15 @@ public class SmallCityService {
 
   public List<Listing> getBusinesses() {
     return businesses;
+  }
+
+  private void addListingToBusinesses(PlacesSearchResult place) {
+    String name = place.name;
+    Geometry geometry = place.geometry;
+    MapLocation placeLocation = new MapLocation(geometry.location.lat, geometry.location.lng);
+    double rating = place.rating;
+    Photo photos[] = place.photos;
+    String types[] = place.types;
+    businesses.add(new Listing(name, placeLocation, rating, photos, types));
   }
 }
