cd2351b769fae111fdbcca10860fe5a81b2c95b5
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-07-10 14:45:24 +0000

    Complete tests

--- src/test/java/com/google/sps/UserTest.java
+++ src/test/java/com/google/sps/UserTest.java
@@ -14,12 +14,14 @@ import com.google.sps.data.Listing;
 import com.google.sps.data.MapLocation;
 import com.google.sps.data.User;
 
+/** Test class for User Object **/
 @RunWith(JUnit4.class)
 public final class UserTest {
   // Pittsburgh Google Office Location
   private final MapLocation USER_MAP_LOCATION = new MapLocation(40.45717, -79.91669);
   private final String USER_ZIPCODE = "15206";
   private final String USER_CITY = "Pittsburgh";
+  private final MapLocation NULL_ISLAND = new MapLocation(0, 0);
   
   private User userWithMapLocation;
   private User userWithZipCode;
@@ -44,7 +46,9 @@ public final class UserTest {
     MapLocation actual = userWithZipCode.getGeolocation();
     MapLocation expected = USER_MAP_LOCATION;
     boolean inRange = false;
-    if ((Math.abs(actual.lat - expected.lat) <= 0.06) && (Math.abs(actual.lng - expected.lng)) <= 0.05) {
+    // Approximate range of lat and lng coordinates in 15206 ZipCode
+    if ((Math.abs(actual.lat - expected.lat) <= 0.06) 
+          && (Math.abs(actual.lng - expected.lng)) <= 0.05) {
       inRange = true;
     }
     Assert.assertTrue(inRange);
@@ -55,7 +59,9 @@ public final class UserTest {
     MapLocation actual = userWithCity.getGeolocation();
     MapLocation expected = USER_MAP_LOCATION;
     boolean inRange = false;
-    if ((Math.abs(actual.lat - expected.lat) <= 0.16) && (Math.abs(actual.lng - expected.lng)) <= 0.2) {
+    // Approximate range of lat and lng coordinates in Pittsburgh
+    if ((Math.abs(actual.lat - expected.lat) <= 0.16) 
+          && (Math.abs(actual.lng - expected.lng)) <= 0.2) {
       inRange = true;
     }   
     Assert.assertTrue(inRange);
