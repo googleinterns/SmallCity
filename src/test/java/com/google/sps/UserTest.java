5f49df0dde27e282de6c5213de3bb921fd8d1109
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-07-10 18:36:39 +0000

    Change variable names

--- src/test/java/com/google/sps/UserTest.java
+++ src/test/java/com/google/sps/UserTest.java
@@ -18,37 +18,31 @@ import com.google.sps.data.User;
 @RunWith(JUnit4.class)
 public final class UserTest {
   // Pittsburgh Google Office Location
-  private final MapLocation USER_MAP_LOCATION = new MapLocation(40.45717, -79.91669);
-  private final String USER_ZIPCODE = "15206";
-  private final String USER_CITY = "Pittsburgh";
-  private final String USER_ADDRESS = "6425 Penn Ave, Pittsburgh, PA 15206";
+  private final MapLocation TEST_USER_MAP_LOCATION = new MapLocation(40.45717, -79.91669);
+  private final String TEST_USER_ZIPCODE = "15206";
+  private final String TEST_USER_CITY = "Pittsburgh";
+  private final String TEST_USER_ADDRESS = "6425 Penn Ave, Pittsburgh, PA 15206";
   
-  private User userWithMapLocation;
-  private User userWithZipCode;
-  private User userWithCity;
-  private User userWithAddress;
-
-  @Before
-  public void setUp() {
-    userWithMapLocation = new User(USER_MAP_LOCATION);
-    userWithZipCode = new User(USER_ZIPCODE);
-    userWithCity = new User(USER_CITY);
-    userWithAddress = new User(USER_ADDRESS);
-  }
+  private User test_userWithMapLocation;
+  private User test_userWithZipCode;
+  private User test_userWithCity;
+  private User test_userWithAddress;
 
   @Test
   public void geolocationUserWithMapLocation() {
-    MapLocation actual = userWithMapLocation.getGeolocation();
-    MapLocation expected = USER_MAP_LOCATION;
+    test_userWithMapLocation = new User(TEST_USER_MAP_LOCATION);
+    MapLocation actual = test_userWithMapLocation.getGeolocation();
+    MapLocation expected = TEST_USER_MAP_LOCATION;
     Assert.assertEquals(expected, actual);
   }
 
   @Test
   public void geolocationUserWithZipCode() {
-    MapLocation actual = userWithZipCode.getGeolocation();
-    MapLocation expected = USER_MAP_LOCATION;
+    test_userWithZipCode = new User(TEST_USER_ZIPCODE);
+    MapLocation actual = test_userWithZipCode.getGeolocation();
+    MapLocation expected = TEST_USER_MAP_LOCATION;
     boolean inRange = false;
-    // Approximate range of lat and lng coordinates in 15206 ZipCode
+    // Approximate range of lat and lng coordinate difference of corners in 15206 ZipCode
     if ((Math.abs(actual.lat - expected.lat) <= 0.06) 
           && (Math.abs(actual.lng - expected.lng)) <= 0.05) {
       inRange = true;
@@ -58,10 +52,11 @@ public final class UserTest {
 
   @Test
   public void geolocationUserWithCity() {
-    MapLocation actual = userWithCity.getGeolocation();
-    MapLocation expected = USER_MAP_LOCATION;
+    test_userWithCity = new User(TEST_USER_CITY);
+    MapLocation actual = test_userWithCity.getGeolocation();
+    MapLocation expected = TEST_USER_MAP_LOCATION;
     boolean inRange = false;
-    // Approximate range of lat and lng coordinates in Pittsburgh
+    // Approximate range of lat and lng coordinate difference of corners in Pittsburgh
     if ((Math.abs(actual.lat - expected.lat) <= 0.16) 
           && (Math.abs(actual.lng - expected.lng)) <= 0.2) {
       inRange = true;
@@ -71,8 +66,9 @@ public final class UserTest {
 
   @Test
   public void geolocationUserWithAddress() {
-    MapLocation actual = userWithAddress.getGeolocation();
-    MapLocation expected = USER_MAP_LOCATION;
+    test_userWithAddress = new User(TEST_USER_ADDRESS);
+    MapLocation actual = test_userWithAddress.getGeolocation();
+    MapLocation expected = TEST_USER_MAP_LOCATION;
     boolean inRange = false;
     // Small range of error allowed for address geolocation
     if ((Math.abs(actual.lat - expected.lat) <= 0.001) 
