973e9d697295765d24327afd1b5227cae8c01725
AbakirH <abakir@google.com>
2020-07-17 11:04:08 +0000

    added code to check if a businnness is a big company

--- src/main/java/com/google/sps/data/BusinessesService.java
+++ src/main/java/com/google/sps/data/BusinessesService.java
@@ -25,6 +25,18 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.maps.TextSearchRequest;
+import com.google.api.services.customsearch.model.Search;
+import com.google.api.services.customsearch.model.Result;
+import com.google.api.services.customsearch.Customsearch;
+import com.google.api.services.customsearch.CustomsearchRequestInitializer;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import java.security.GeneralSecurityException;
+import java.io.IOException;
+import java.util.Map;
+import java.lang.Integer;
+
 
 /** BusinessesService object representing all businesses 
 * components of the webapp.
@@ -36,6 +48,9 @@ public class BusinessesService {
   private final static Logger LOGGER = 
         Logger.getLogger(BusinessesService.class.getName());
   private final int ALLOWED_SEARCH_REQUESTS = 3;
+  private Listing currentBusiness;
+  private PlacesSearchResponse similarBusinessesInTheArea;
+  private LatLng latLng;
 
   /** Create a new Businesses instance
   * @param allBusinesses businesses from SmallCityService
@@ -71,7 +86,7 @@ public class BusinessesService {
   }
   
   public List<Listing> getBusinessesFromPlacesApi(User user) {
-    LatLng latLng = 
+    latLng = 
           new LatLng(user.getGeolocation().lat, user.getGeolocation().lng);
     final GeoApiContext context = new GeoApiContext.Builder()
             .apiKey(KEY)
@@ -109,5 +124,102 @@ public class BusinessesService {
     String types[] = place.types;
     allBusinesses.add(new Listing(name, formattedAddress, 
           placeLocation, rating, photos, types));
+  }
+
+  public void checkNumberOfLocationsOfBusiness() {
+    GeoApiContext context = new GeoApiContext.Builder()
+            .apiKey(KEY)
+            .build();
+    try {
+      for(int i = 0; i<30;i++){
+        currentBusiness = allBusinesses.get(i);
+        TextSearchRequest request = new TextSearchRequest(context)
+                                            .query(currentBusiness.getName())
+                                            .location(latLng)
+                                            .radius(10000);
+        similarBusinessesInTheArea = request.await();
+        if(similarBusinessesInTheArea.results.length > 1){
+          checkBusinessThroughLinkedin(currentBusiness.getName());
+        }
+      }
+
+    }catch(Exception e) {
+      LOGGER.warning(e.getMessage());
+    }  
+  }
+  
+  private void checkBusinessThroughLinkedin(String currentBusinessName) 
+                          throws GeneralSecurityException, IOException {
+    String cx = "001390425498086947771:mhzyevhmmxq"; //Your search engine
+    //Instance Customsearch
+    Customsearch cs = new Customsearch.Builder(
+                                GoogleNetHttpTransport.newTrustedTransport(), 
+                                JacksonFactory.getDefaultInstance(), 
+                                null) 
+                                .setApplicationName("linkedinSearch") 
+                                .setGoogleClientRequestInitializer(
+                                      new CustomsearchRequestInitializer(KEY)) 
+                                .build();
+    Customsearch.Cse.List list = cs.cse().list(currentBusinessName).setCx(cx); 
+    List<Result> searchJsonResults = list.execute().getItems();
+    String[] numberOfFollowers;
+    if (searchJsonResults!=null){
+      if(searchJsonResults.size() != 0){
+        Result linkedinBusiness = searchJsonResults.get(0);
+        List<Map<String, Object>> resultsMetatags = 
+                        (List) linkedinBusiness.getPagemap().get("metatags");
+        for (Map<String, Object> tag : resultsMetatags) {
+          String b_title = (String) tag.get("og:description");
+          numberOfFollowers = b_title.split(" ");
+          findNumberOfFollowers(numberOfFollowers);
+        }
+      }
+    }
+  }
+
+  private void findNumberOfFollowers(String[] numberOfFollowers) {
+    int companyFollowers = 0;
+    for(int i = 0; i < numberOfFollowers.length; i++) {
+      if(numberOfFollowers[i].equals("followers")) {
+        companyFollowers = 
+                Integer.parseInt(numberOfFollowers[i-1].replaceAll(",", ""));
+      }
+    }
+    if(companyFollowers > 50000) {
+      addBigBusinessToDatabase();
+    }else {
+      checkNumberOfSimilarBusinessesInTheArea(currentBusiness.getName());
+    }
+  }
+
+  private void checkNumberOfSimilarBusinessesInTheArea(String businessName){
+    int countNumberOfMatchingBusiness = 0;
+    int i = 0;
+    while(i < similarBusinessesInTheArea.results.length 
+          && countNumberOfMatchingBusiness < 10) {
+      if(similarBusinessesInTheArea.results[i].name.contains(businessName)) {
+        countNumberOfMatchingBusiness++;
+      }
+      i++;
+     }
+     if(countNumberOfMatchingBusiness >= 10) {
+       addBigBusinessToDatabase();
+     }
+   }
+
+  private void addBigBusinessToDatabase(){
+    String title = "Business";
+    String businessTypes = "BusinessTypes";
+    String address = "Address";
+    String rating = "Rating";
+    String photos = "Photos";
+    Entity businessEntity = new Entity("BigBusinesses");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    businessEntity.setProperty(title, currentBusiness.getName());
+    businessEntity.setProperty(address, currentBusiness.getFormattedAddress());
+    businessEntity.setProperty(rating, currentBusiness.getRating());
+    businessEntity.setProperty(businessTypes, 
+                            Arrays.asList(currentBusiness.getBusinessTypes()));
+    datastore.put(businessEntity);
   }
 }
