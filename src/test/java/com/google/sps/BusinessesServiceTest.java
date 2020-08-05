d6d4ba92f4a3d3217898cbd8216e19039713a9a3
Lindsey Weiskopf <ltweiskopf@gmail.com>
2020-08-05 16:40:07 +0000

    Fix bug in test

--- src/test/java/com/google/sps/BusinessesServiceTest.java
+++ src/test/java/com/google/sps/BusinessesServiceTest.java
@@ -1,90 +1,98 @@
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Arrays;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import com.google.sps.data.SmallCityService;
-import com.google.sps.data.UserService;
+import com.google.sps.data.BusinessesService;
 import com.google.sps.data.Listing;
 import com.google.sps.data.MapLocation;
-import com.google.sps.data.SearchObject;
-import com.google.sps.data.User;
-import com.google.maps.model.Geometry;
-import static org.Mockito.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(JUnit4.class)
 public final class BusinessesServiceTest {
-/*
-  private List<Listing> testBusinesses = new LinkedList<Listing>();
-  private BusinessesService testBusinessesService = new BusinessesService(testBusinesses);
-  private PlaceSearchResult TEST_PLACE_SEARCH_RESULT = new PlaceSearchResult();
-  TEST_PLACE_SEARCH_RESULT.name = "TEST_NAME";
-  TEST_PLACE_SEARCH_RESULT.vicinity = "TEST_VICINITY";
-  TEST_PLACE_SEARCH_RESULT.formattedAddress = "TEST_FORMATTED_ADDRESS";
-  TEST_PLACE_SEARCH_RESULT.geometry = new Geometry();
-  TEST_PLACE_SEARCH_RESULT.rating = 0;
-  TEST_PLACE_SEARCH_RESULT.photos = null;
-  TEST_PLACE_SEARCH_RESULT.types = null;
-  */
+
   private final int MIN_LISTINGS_RETURNED = 20;
   private final int MAX_LISTINGS_RETURNED = 60;
-
+  
+  // Address location in Owings Mills, MD
   private final MapLocation TEST_MAP_LOCATION = new MapLocation(39.459836, -76.742536);
   private final String TEST_PRODUCT = "pizza";
 
   private final List<Listing> TEST_PASSED_LIST = new LinkedList<>();
 
-  private List<Listing> TEST_TEXT_SEARCH_RESULT = new LinkedList<>(Arrays.asList(new Listing("Pizza Connection", 
-        "11215 York Rd, Cockeysville, MD 21030, United States", 
-        null,
-        4.4,
-        null,
-        null), 
-        new Listing("Brookside's Pizzeria",
-        "9419 Common Brook Rd #109, Owings Mills, MD 21117, United States",
-        null,
-        4.2,
-        null,
-        null),
-        new Listing("Village Pizza",
-        "38 Main St, Reisterstown, MD 21136, United States",
-        null,
-        4.2,
-        null,
-        null)));
-
-
-@Test
-public void testGetBusinessesFromTextSearch() {
-  BusinessesService mockedBusinessesService = mock(BusinessesService.class);
-  BusinessesService testBusinessesService = new BusinessesService(TEST_PASSED_LIST);
-  when(mockedBusinessesService.getBusinessesFromTextSearch(TEST_MAP_LOCATION, TEST_PRODUCT))
-        .thenReturn(TEST_TEXT_SEARCH_RESULT); 
-
-  List<Listing> expected = mockedBusinessesService
-        .getBusinessesFromTextSearch(TEST_MAP_LOCATION, TEST_PRODUCT);
-  List<Listing> actual = testBusinessesService
-        .getBusinessesFromTextSearch(TEST_MAP_LOCATION, TEST_PRODUCT);
-
-  Assert.assertTrue(actual.size() >= MIN_LISTINGS_RETURNED);
-  Assert.assertTrue(actual.size() <= MAX_LISTINGS_RETURNED);
+  private BusinessesService mockedBusinessesService = mock(BusinessesService.class);
+  private BusinessesService testBusinessesService = new BusinessesService(TEST_PASSED_LIST);
   
-  boolean containsExpected = false;
-  for (Listing expectedListing : expected) {
-    containsExpected = false;
-    for (Listing actualListing : actual) {
-      if (expectedListing.name == actualListing.name) {
-        containsExpected = true;
-        break;
+  // 3 known returned businesses
+  private List<Listing> TEST_TEXT_SEARCH_RESULT = 
+        new LinkedList<>(Arrays.asList(new Listing("Pizza Connection", 
+        "11215 York Rd, Cockeysville, MD 21030, United States", null,
+        4.4, null, null), 
+        new Listing("Brookside's Pizzeria",
+        "9419 Common Brook Rd #109, Owings Mills, MD 21117, United States", null,
+        4.2, null, null),
+        new Listing("Village Pizza",
+        "38 Main St, Reisterstown, MD 21136, United States", null,
+        4.2, null, null)));
+  
+  // 3 known returned businesses
+  private List<Listing> TEST_NEARBY_SEARCH_RESULT = 
+        new LinkedList<>(Arrays.asList(new Listing("Perfect Windows Inc",
+        "2927 Walnut Avenue, Owings Mills", null,
+        0.0, null, null),
+        new Listing("Fortuna Liquor & Food",
+        "12147 Park Heights Avenue, Owings Mills", null,
+        0.0, null, null),
+        new Listing("Dantech, Inc",
+        "12149 Park Heights Avenue, Owings Mills", null,
+        5.0, null, null)));
+
+  @Test
+  public void testGetBusinessesFromTextSearch() {
+    when(mockedBusinessesService.getBusinessesFromTextSearch(TEST_MAP_LOCATION, TEST_PRODUCT))
+          .thenReturn(TEST_TEXT_SEARCH_RESULT); 
+
+    List<Listing> expected = mockedBusinessesService
+          .getBusinessesFromTextSearch(TEST_MAP_LOCATION, TEST_PRODUCT);
+    List<Listing> actual = testBusinessesService
+          .getBusinessesFromTextSearch(TEST_MAP_LOCATION, TEST_PRODUCT);
+
+    Assert.assertTrue(actual.size() >= MIN_LISTINGS_RETURNED);
+    Assert.assertTrue(actual.size() <= MAX_LISTINGS_RETURNED);
+    
+    int containsCounter = 0;
+    for (Listing expectedListing : expected) {
+      for (Listing actualListing : actual) {
+        if (expectedListing.getName().equals(actualListing.getName())) {
+          containsCounter++;
+        }
       }
     }
-    if (containsExpected == false) {
-      break;
-    }
+    Assert.assertEquals(containsCounter, expected.size());
   }
 
-  Assert.assertTrue(containsExpected);
-}
+  @Test
+  public void testGetBusinessesFromNearbySearch() {
+    when(mockedBusinessesService.getBusinessesFromNearbySearch(TEST_MAP_LOCATION))
+          .thenReturn(TEST_NEARBY_SEARCH_RESULT); 
+    List<Listing> expected = mockedBusinessesService
+          .getBusinessesFromNearbySearch(TEST_MAP_LOCATION);
+    List<Listing> actual = testBusinessesService
+          .getBusinessesFromNearbySearch(TEST_MAP_LOCATION);
+
+    Assert.assertTrue(actual.size() >= MIN_LISTINGS_RETURNED);
+    Assert.assertTrue(actual.size() <= MAX_LISTINGS_RETURNED);
+
+    int containsCounter = 0;
+    for (Listing expectedListing : expected) {
+      for (Listing actualListing : actual) {
+        if (expectedListing.getName().equals(actualListing.getName())) {
+          containsCounter++;
+        }
+      }
+    }
+    Assert.assertEquals(containsCounter, expected.size());
+  }
 }
